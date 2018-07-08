97b4b34696e8c78f48fcdede45d93529bf4119b4
Malte <matle.fillmer@googlemail.com>
2018-07-08 19:27:52 +0200

    moved constants to actions folder

--- src/actions/constants.js
+++ src/actions/constants.js
@@ -1,12 +1,16 @@
 function my_enum(...args) {
     return args.reduce(function (obj, arg, index) {
-        obj[arg] = index;
+        obj[arg] = arg;
         return obj;
     }, {});
 }
 
 const C = my_enum(
-    "MY_ACTION"
+    "ADD_WEIGHT",
+    "ADD_EXERCISE",
+    "CHANGE_WRAPS",
+    "CHANGE_SETS",
+    "ADD_WEEKDAY"
 );
 
 export default C;
