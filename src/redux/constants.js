062e93adab6fa4eb5e112153b233a2f3363d2dff
Malte <matle.fillmer@googlemail.com>
2018-07-09 14:43:54 +0200

    modify constants file
    enums are not suitable

--- src/redux/constants.js
+++ src/redux/constants.js
@@ -1,16 +1,9 @@
-function my_enum(...args) {
-    return args.reduce(function (obj, arg, index) {
-        obj[arg] = arg;
-        return obj;
-    }, {});
-}
-
-const C = my_enum(
-    "ADD_WEIGHT",
-    "ADD_EXERCISE",
-    "CHANGE_WRAPS",
-    "CHANGE_SETS",
-    "ADD_WEEKDAY"
-);
+const C = {
+    ADD_WEIGHT: "ADD_WEIGHT",
+    ADD_EXERCISE: "ADD_EXERCISE",
+    CHANGE_WRAPS: "CHANGE_WRAPS",
+    CHANGE_SETS: "CHANGE_SETS",
+    ADD_WEEKDAY: "ADD_WEEKDAY"
+};
 
 export default C;
