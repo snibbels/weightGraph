5c15c40547236d0bf95d3192d0532eff7813f0e4
Malte <matle.fillmer@googlemail.com>
2018-07-11 15:53:19 +0200

    move edit button from exercise to weekday and hide it on all screens

--- src/weekplan/WeekDay.js
+++ src/weekplan/WeekDay.js
@@ -1,25 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Exercise from './Exercise';
+import { FaEdit } from 'react-icons/lib/fa';
+import '../custom.css';
+import { addWeight } from '../redux/actions'
 
-const WeekDay = ({ name, exercises, store }) => (
-    <React.Fragment>
-        <h3>{name}</h3>
-        <table className="w3-table w3-responsive">
-            <tbody>
-                <tr>
-                    <th>Übung</th>
-                    <th>Sätze</th>
-                    <th>Wdh</th>
-                    <th>Gewicht</th>
-                </tr>
-                {
-                    exercises.map((e, i) => (
-                        <Exercise key={i} {...e} store={store} />
-                    ))
-                }
-            </tbody>
-        </table>
-    </React.Fragment>
-)
+export default class WeekDay extends Component {
+    constructor(props) {
+        super(props);
 
-export default WeekDay;
+        this.state = {
+            editmode: false
+        }
+
+        this.toggleEditmode = this.toggleEditmode.bind(this);
+        this.editWeight = this.editWeight.bind(this);
+    }
+
+    toggleEditmode() {
+        this.setState({ editmode: !this.state.editmode });
+    }
+
+    editWeight(weight = 0, id) {
+        if (!weight) return;
+        this.props.store.dispatch(
+            addWeight(id, weight, Date.now())
+        );
+        this.setState({ editmode: false });
+    }
+
+    render() {
+        const { name, exercises, store } = this.props;
+        return (
+
+            <div >
+                <span className="w3-bar hidden-container" onClick={this.toggleEditmode}>
+                    <h3 className="w3-bar-item">{name}</h3>
+                    <h3 className="w3-bar-item hidden"
+                        style={{ visibility: (this.state.editmode) ? "visible" : "" }}
+                    >
+                        <FaEdit />
+                    </h3>
+                </span>
+                <table className="w3-table">
+                    <tbody>
+                        <tr>
+                            <th>Übung</th>
+                            <th>Sätze</th>
+                            <th>Wdh</th>
+                            <th>Gewicht</th>
+                        </tr>
+                        {
+                            exercises.map((e, i) => (
+                                <Exercise
+                                    key={i}
+                                    {...e}
+                                    store={store}
+                                    editmode={this.state.editmode}
+                                    editWeight={this.editWeight}
+                                />
+
+                            ))
+                        }
+                    </tbody>
+                </table>
+            </div >
+        )
+    }
+}
