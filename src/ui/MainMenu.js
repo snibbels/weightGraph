fb414994666e224852bf7c7ad16de60e2e71144c
Malte <matle.fillmer@googlemail.com>
2018-07-17 13:13:55 +0200

    add custom tab icons

--- src/ui/MainMenu.js
+++ src/ui/MainMenu.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { FaTrash } from 'react-icons/lib/fa'
+import { FaTrash } from 'react-icons/lib/fa';
+import chart from '../icons/chart.svg';
+import dumbbell from '../icons/dumbbell.svg';
 
 const activeStyle = {
     borderBottom: "4px solid #2395F3"
+}
+
+const icon = {
+    width: "1.5em",
+    margin: "0.5em"
 }
 
 const MainMenu = () => (
@@ -12,15 +19,25 @@ const MainMenu = () => (
             activeStyle={activeStyle}
             className="w3-bar-item w3-button"
             exact
-            to="/">Mein Trainingsplan</NavLink>
+            to="/">
+            <img src={dumbbell} style={icon} alt="" />
+            <span className="w3-hide-small">
+                Mein Trainingsplan
+            </span>
+        </NavLink>
         <NavLink
             activeStyle={activeStyle}
             className="w3-bar-item w3-button"
-            to="/stats">Meine Statistik</NavLink>
+            to="/stats">
+            <img src={chart} style={icon} alt="" />
+            <span className="w3-hide-small">
+                Meine Statistik
+            </span>
+        </NavLink>
         <div
             onClick={localStorage.clear('localWeights')}
             className="w3-bar-item w3-button">
-            <FaTrash />
+            <FaTrash style={icon} />
         </div>
     </div>
 )
