8443634ce7253d9bcd6ed7369e44abcc283e417d
Malte <matle.fillmer@googlemail.com>
2018-07-27 15:09:12 +0200

    adjust styling

--- src/workouts/Exercise.js
+++ src/workouts/Exercise.js
@@ -1,7 +1,8 @@
 import React from 'react'
 
 const Exercise = ({ name, muscles }) => (
-    <div className="w3-card w3-margin w3-padding w3-large">
+    <div className="w3-card w3-margin w3-padding w3-large
+        w3-col s11 m4 l3">
         {name}
     </div>
 );
