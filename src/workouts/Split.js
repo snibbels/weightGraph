e035fb940e0d8aa37feb0e5ca59a6c587d34c87d
Malte <matle.fillmer@googlemail.com>
2018-07-27 16:05:58 +0200

    selected exercises are displayed under their split

--- src/workouts/Split.js
+++ src/workouts/Split.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import SplitItem from './SplitItem';
+import StoreComponent from '../HOCs/StoreComponent'
 
 
-const Split = ({ name, exercises = [], className, style }) => (
-    <div className={className} style={style}>
-        <h4>{name}</h4>
-        <ul className="w3-ul">
-            {exercises.map((e, i) => (
-                <SplitItem {...e} key={i}
-                    className="w3-display-container w3-hover-blue" />
-            ))}
-        </ul>
-    </div>
-);
+const _Split = ({ name, muscles, className, style, store }) => {
+    const exercises = store.getState().exercises.filter(
+        e => (e.selected && !!muscles.find(
+            muscle => (e.muscles.indexOf(muscle) > -1)
+        ))
+    );
 
+    return (
+        <div className={className} style={style}>
+            <h4>{name}</h4>
+            <ul className="w3-ul">
+                {exercises.map((e, i) => (
+                    <SplitItem {...e} key={i}
+                        className="w3-display-container w3-hover-blue" />
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+const Split = StoreComponent(_Split);
 
 export default Split;
