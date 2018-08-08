1f13b6ff602b1bb9ef65d92313e57397a6efdeb0
malte <malte.fillmer@googlemail.com>
2018-08-08 21:52:19 +0200

    bundle styling in mother ui component

---
+++ src/ui/FlexCardRow.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export const cardStyleClasses = ["w3-card", "w3-left-align", "w3-padding", "w3-display-container"].join(' ');
+export const flexCardRow = ["w3-row"].join(' ');
+export const flexCardContainer = ["w3-col", "s12 m6 l4", "w3-padding"].join(' ');
+
+const FlexCardRow = ({ children }) => (
+    <div className={flexCardRow}>
+        {
+            children.map(
+                c => (
+                    <div className={flexCardContainer}>
+                        {c}
+                    </div>
+                )
+            )
+        }
+    </div>
+);
+
+export default FlexCardRow;
