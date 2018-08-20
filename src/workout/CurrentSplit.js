6965a9b6e382f74a8b0d354fc4bcba23cbf9d380
malte <malte.fillmer@googlemail.com>
2018-08-20 20:09:18 +0200

    hotfix

--- src/workout/CurrentSplit.js
+++ src/workout/CurrentSplit.js
@@ -4,12 +4,19 @@ import ExerciseItem from './ExerciseItem';
 
 const CurrentSplit = ({ split, exercises, exerciseId, history = [] }) => {
     if (!split) return null;
-    const currentExercises = exercises.filter(
-        e => split.exercises.indexOf(e.id) > -1
-    ).map(e => ({
-        ...history.find(h => h.exerciseId === e.id),
-        ...e,
-    }));
+    // const currentExercises = exercises.filter(
+    //     e => split.exercises.indexOf(e.id) > -1
+    // ).map(e => ({
+    //     ...history.find(h => h.exerciseId === e.id),
+    //     ...e,
+    // }));
+    const currentExercises = split.exercises.map(
+        e => ({
+            ...exercises.find(ex => ex.id === e),
+            ...history.find(h => h.exerciseId === e)
+        })
+    )
+
 
     return (
         <div className={cardStyleClasses}>
--- src/workout/ExerciseItem.js
+++ src/workout/ExerciseItem.js
@@ -3,7 +3,7 @@ import { muscleColors } from '../redux/constants';
 
 const ExerciseItem = ({ current = false, muscles = [], name, weight }) => (
     <li style={{ fontWeight: current ? "bold" : "normal" }}>
-        <span style={{ width: "90%", display: "inline-block" }}>
+        <span style={{ width: "85%", display: "inline-block" }}>
             {muscles.map((m, i) => (
                 <span
                     key={i}
@@ -21,7 +21,7 @@ const ExerciseItem = ({ current = false, muscles = [], name, weight }) => (
                 {name}
             </span>
         </span>
-        <span style={{ textAlign: "right", width: "10%", display: "inline-block" }}>
+        <span style={{ textAlign: "right", width: "15%", display: "inline-block" }}>
             {weight} {weight !== undefined ? "kg" : ""}
         </span>
     </li>
