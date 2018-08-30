ba41c1eb3feb47ed593616955881a7ac88d112ae
malte <malte.fillmer@googlemail.com>
2018-08-30 17:00:26 +0200

    play around with d3

--- src/stats/Statistics.js
+++ src/stats/Statistics.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import FlexCardRow, { cardStyleClasses } from '../ui/FlexCardRow';
 import AverageMuscleWeights from './AverageMuscleWeights';
+import Test from './Test'
 
 const Statistics = () => (
     <FlexCardRow>
+        <Test className={cardStyleClasses} />
         <AverageMuscleWeights className={cardStyleClasses} />
     </FlexCardRow>
 )
