ab989b410ea22104f8b0b873c2ec1ceee4efd063
snibbels <malte.fillmer@googlemail.com>
2019-10-25 16:54:16 +0200

    Updates

---
+++ precache-manifest.ef3d844bae780316902b1423e53e9f96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2a96363058b8b5602d69e04dc60e27c",
+    "url": "/weightGraph/index.html"
+  },
+  {
+    "revision": "c8146f1378ffa0b708df",
+    "url": "/weightGraph/static/css/2.823a9121.chunk.css"
+  },
+  {
+    "revision": "9f046e22a61f64e30f87",
+    "url": "/weightGraph/static/css/main.c11535e0.chunk.css"
+  },
+  {
+    "revision": "c8146f1378ffa0b708df",
+    "url": "/weightGraph/static/js/2.672d635d.chunk.js"
+  },
+  {
+    "revision": "9f046e22a61f64e30f87",
+    "url": "/weightGraph/static/js/main.c4730f32.chunk.js"
+  },
+  {
+    "revision": "cf50e9868061474c1c6f",
+    "url": "/weightGraph/static/js/runtime-main.0b1b948a.js"
+  }
+]);
